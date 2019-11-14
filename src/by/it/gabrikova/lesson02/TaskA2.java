a75882862e662b21f59b4a6a2de9d446fb771217
Gabriellji <gabriellji@yahoo.com>
2019-11-14 12:08:31 +0300

    added lesson2 TaskA complete

--- src/by/it/gabrikova/lesson02/TaskA2.java
+++ src/by/it/gabrikova/lesson02/TaskA2.java
@@ -9,5 +9,13 @@ package by.it.gabrikova.lesson02;
 */
 
 class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+
+            System.out.println("Я начинаю изучать Java!");
+
+        }
+
+    }
 
 }
