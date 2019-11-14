877f429c54e05cf4b5fab3c5e91beb5b33d52daf
Виктор <lucckyboy713@gmail.com>
2019-11-14 12:11:06 +0300

    Comment

--- src/by/it/givi/TaskA3.java
+++ src/by/it/givi/TaskA3.java
@@ -7,9 +7,9 @@ package by.it.givi;
 
 class TaskA3 {
     public static void main(String[] args) {
-        int i = 2;
-        int j = 3;
-        int k = i + j;
-        System.out.println(i + "+" + j + "=" + k);
+        int i = 3;
+        int j = 4;
+        int k = 3*3+4*4;
+        System.out.println( i * i + "+" + j * j + "="+ k);
     }
 }
