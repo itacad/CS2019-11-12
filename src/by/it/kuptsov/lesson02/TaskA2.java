c7ca28ea8aa2f6cebd658ef592c76d772c57bf75
kuptsov <akuptsovvv@gmail.com>
2019-11-14 12:09:06 +0300

    add lesson2 task a complete

--- src/by/it/kuptsov/lesson02/TaskA2.java
+++ src/by/it/kuptsov/lesson02/TaskA2.java
@@ -9,5 +9,10 @@ package by.it.kuptsov.lesson02;
 */
 
 class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
 
+        }
+            System.out.println("Я начинаю изучать Java!");
+    }
 }
