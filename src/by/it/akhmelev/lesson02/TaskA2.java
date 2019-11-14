b2a3b8e85109eea3705b5b021573a22f0db698f3
loktev <loktev@tut.by>
2019-11-14 12:08:26 +0300

    lesson2 TaskA complete

--- src/by/it/akhmelev/lesson02/TaskA2.java
+++ src/by/it/akhmelev/lesson02/TaskA2.java
@@ -9,5 +9,10 @@ package by.it.akhmelev.lesson02;
 */
 
 class TaskA2 {
+    public static void main(String[] args) {
+        for (int i = 0; i <5; i++) {
+            System.out.println("Я начинаю изучать Java!");
+        }
+    }
 
 }
