b99861e99ea5155be9d13a4a119edbb5271b7a8d
Виктор <lucckyboy713@gmail.com>
2019-11-17 12:05:10 +0300

    Comment

--- src/by/it/givi/TaskC1.java
+++ src/by/it/givi/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.givi;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -11,9 +13,10 @@ Sum = 60
 
 */
 class TaskC1 {
-
-
-
-
-
+    public static void main(String[] args) {
+        Scanner O = new Scanner(System.in);
+        int q = O.nextInt();
+        int e = O.nextInt();
+        System.out.print(q + e);
+    }
 }
