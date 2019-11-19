99df8685bac251d039e6197179ed770167e39af3
admin <abc@gmail.com>
2019-11-19 12:13:03 +0300

    lesson3 A

--- src/by/it/popkov/lesson03/TaskA2.java
+++ src/by/it/popkov/lesson03/TaskA2.java
@@ -24,10 +24,10 @@ class TaskA2 {
     public static void main(String[] args) {
         System.out.println("Мое любимое стихотворение:");
         String a = "У лукоморья дуб зелёный;";
-        String b ="";
-        String c ="";
-        String d ="";
-        System.out.println();
+        String b = "Златая цепь на дубе том:";
+        String c = "И днём и ночью кот учёный";
+        String d = "Всё ходит по цепи кругом;";
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + d + "\n");
     }
 
 }
