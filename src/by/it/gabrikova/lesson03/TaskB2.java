0a41ad4305f7c2bcc05e9e1ca8195dc2443c3d7c
Gabriellji <gabriellji@yahoo.com>
2019-11-21 00:12:17 +0300

    lesson3 TaskB2 complete

--- src/by/it/gabrikova/lesson03/TaskB2.java
+++ src/by/it/gabrikova/lesson03/TaskB2.java
@@ -32,17 +32,30 @@ Lesson 03. Task B2. Нужно написать программу, котора
 
 */
 class TaskB2 {
-    static double dis(int a, int b, int c) {
+    private static double dis(int a, int b, int c) {
         return b * b - 4 * a * c;
     }
 
 
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         int a = scanner.nextInt();
-         int b = scanner.nextInt();
-         int c = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
 
+        double d = dis(a, b, c);
+        if (d > 0) {
+            double x1, x2;
+            x1 = (-b - Math.sqrt(d)) / (2 * a);
+            x2 = (-b + Math.sqrt(d)) / (2 * a);
+            System.out.println(x1 + " " + x2);
+        } else if (d == 0) {
+            double x;
+            x = -b / ((double)2 * a);
+            System.out.println(x);
+        } else {
+            System.out.println("Отрицательный дискриминант");
+        }
 
 
     }
