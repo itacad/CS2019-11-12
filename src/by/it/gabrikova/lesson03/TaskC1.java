f071104e49191ca42ea6d5d1203c9cf73c829a1f
Gabriellji <gabriellji@yahoo.com>
2019-11-21 00:35:30 +0300

    lesson3 TaskC1 complete

--- src/by/it/gabrikova/lesson03/TaskC1.java
+++ src/by/it/gabrikova/lesson03/TaskC1.java
@@ -27,11 +27,13 @@ TC = (TF – 32) * 5/9
 class TaskC1 {
 
 
+    static double convertCelsiumToFahrenheit(int celsium) {
+        return (celsium * 9.0 ) / 5.0  + 32;
+    }
 
 
-
-//    public static void main(String[] args) {
-//        System.out.println(convertCelsiumToFahrenheit(40));
-//    }
+    public static void main(String[] args) {
+                System.out.println(convertCelsiumToFahrenheit(40));
+    }
 
 }
