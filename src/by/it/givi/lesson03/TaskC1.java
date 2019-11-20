bba0b6d44639c5b3e7c3f8cea5ac1762f234176f
Виктор <lucckyboy713@gmail.com>
2019-11-20 10:54:52 +0300

    Comment

--- src/by/it/givi/lesson03/TaskC1.java
+++ src/by/it/givi/lesson03/TaskC1.java
@@ -3,40 +3,38 @@ package by.it.givi.lesson03;
 /*
 Lesson 03. Конвертер температуры
 
-Метод convertCelsiusToFahrenheit(int Celsius) принимает значение в градусах Цельсия.
+Метод convertCelsiumToFahrenheit(int Celsium) принимает значение в градусах Цельсия.
 Метод должен переводить температуру и возвращать значение в градусах Фаренгейта.
 
 Температура по Цельсию TC и температура по Фаренгейту TF связаны следующим соотношением:
 TC = (TF – 32) * 5/9
 
 Пример:
-В метод convertCelsiusToFahrenheit на вход подается значение 40.
+В метод convertCelsiumToFahrenheit на вход подается значение 40.
 
 Пример вывода:
 104.0
 
 
 Требования:
-1. Метод convertCelsiusToFahrenheit(int) должен быть не приватным и статическим.
-2. Метод convertCelsiusToFahrenheit должен возвращать значение типа double.
-3. Метод convertCelsiusToFahrenheit не должен ничего выводить на экран.
-4. Метод convertCelsiusToFahrenheit должен правильно переводить градусы Цельсия
+1. Метод convertCelsiumToFahrenheit(int) должен быть не приватным и статическим.
+2. Метод convertCelsiumToFahrenheit должен возвращать значение типа double.
+3. Метод convertCelsiumToFahrenheit не должен ничего выводить на экран.
+4. Метод convertCelsiumToFahrenheit должен правильно переводить градусы Цельсия
 в градусы Фаренгейта и возвращать это число.*/
 
 
-class TaskC1 {
-    public static class C1 {
+    public class TaskC1 {
         public static void main(String[] args) {
-            System.out.println(convertCelsiusToFahrenheit(40));
+            System.out.println(convertCelsiumToFahrenheit(40));
         }
-        public static double convertCelsiusToFahrenheit(int celsius) {
-            double far = 9 / 5.0 * celsius + 32;
-            return far;
+        public static double convertCelsiumToFahrenheit(int TC) {
+            double TF = 9 / 5.0 * TC + 32;
+            return TF;
         }
     }
-}
 //    public static void main(String[] args) {
-//        System.out.println(convertCelsiusToFahrenheit(40));
+//        System.out.println(convertCelsiumToFahrenheit(40));
 //    }
 
 
