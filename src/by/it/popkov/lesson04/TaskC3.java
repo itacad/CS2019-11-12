04d166bc7c07a23890f0124650251fd781b1e181
user <abc>
2019-11-23 12:30:03 +0300

    С3 is done

--- src/by/it/popkov/lesson04/TaskC3.java
+++ src/by/it/popkov/lesson04/TaskC3.java
@@ -23,6 +23,86 @@ package by.it.popkov.lesson04;
 десять умножить на десять равно сто
 */
 
+import java.util.Scanner;
+
 public class TaskC3 {
+    static String numToString(int num){
+        String textNum = "";
+        if (num >= 0 && num < 20){
+            switch (num){
+                case 0: textNum = "ноль"; break;
+                case 1: textNum = "один"; break;
+                case 2: textNum = "два"; break;
+                case 3: textNum = "три"; break;
+                case 4: textNum = "четыре"; break;
+                case 5: textNum = "пять"; break;
+                case 6: textNum = "шесть"; break;
+                case 7: textNum = "семь"; break;
+                case 8: textNum = "восемь"; break;
+                case 9: textNum = "девять"; break;
+                case 10: textNum = "десять"; break;
+                case 11: textNum = "одинадцать"; break;
+                case 12: textNum = "двенадцать"; break;
+                case 13: textNum = "тринадцать"; break;
+                case 14: textNum = "четырнадцать"; break;
+                case 15: textNum = "пятнадцать"; break;
+                case 16: textNum = "шестнадцать"; break;
+                case 17: textNum = "семнадцать"; break;
+                case 18: textNum = "восемнадцать"; break;
+                case 19: textNum = "девятнадцать"; break;
+            }
+
+        }
+        else if (num >=20 && num < 40){
+            if (num != 20 && num != 30){
+            textNum = numToString(num / 10) + "дцать " + numToString(num - ((num/10)*10));
+            }
+            else {
+                textNum = numToString(num / 10) + "дцать";
+            }
+        }
+        else if (num >=40 && num < 50){
+            if (num != 40){
+            textNum = "сорок " + numToString(num - ((num/10)*10));
+            }
+            else {
+                textNum = "сорок";
+            }
+        }
+        else if (num >=50 && num < 90){
+            if (num != 50 && num != 60 && num != 70 && num != 80){
+            textNum = numToString(num / 10) + "десят " + numToString(num - ((num/10)*10));
+            }
+            else {
+                textNum = numToString(num / 10) + "десят";
+            }
+        }
+        else if (num >=90 && num < 100){
+            if (num != 90){
+                textNum = "девяносто " + numToString(num - ((num/10)*10));
+            }
+            else {
+                textNum = "девяносто";
+            }
+        }
+        else if (num == 100){
+            textNum = "сто";
+        }
+        else {
+            textNum = "Недопустимое значение";
+        }
+        return textNum;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int size = scan.nextInt();
+
+        for (int firstMult = 2; firstMult <= size; firstMult++) {
+            for (int secondMult = 2; secondMult <= size; secondMult++) {
+                System.out.println(numToString(firstMult) + " умножить на " + numToString(secondMult) + " равно " + numToString(firstMult * secondMult));
+            }
+
+        }
+    }
 
 }
