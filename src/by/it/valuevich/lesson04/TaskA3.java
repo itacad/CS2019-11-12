d4d7369c3b3b05b493a6b6ae0f38dce0c1597f02
DrCarpediem <57077162+DrCarpediem@users.noreply.github.com>
2019-11-23 14:02:04 +0300

    lesson4 Tasks A3 complete

--- src/by/it/valuevich/lesson04/TaskA3.java
+++ src/by/it/valuevich/lesson04/TaskA3.java
@@ -31,13 +31,13 @@ public class TaskA3 {
         int b = sc.nextInt();
         int c = sc.nextInt();
         if (a == b && b == c) {
-            System.out.println(a + " " + b + "" + c);
+            System.out.println(a + " " + b + " " + c);
         } else if (a == b) {
             System.out.println(a + " " + b);
         } else if (a == c) {
             System.out.println(a + " " + c);
-        } else if (b ==§§ c) {
-            System.out.println(a + " " + c);
+        } else if (b == c) {
+            System.out.println(b + " " + c);
         }
 
     }
