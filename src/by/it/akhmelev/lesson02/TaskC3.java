1c36a7f590ca381675b27073d637c1c8f4e3aabf
loktev <loktev@tut.by>
2019-11-26 11:50:24 +0300

    lesson 4 level A

--- src/by/it/akhmelev/lesson02/TaskC3.java
+++ src/by/it/akhmelev/lesson02/TaskC3.java
@@ -1,5 +1,7 @@
 package by.it.akhmelev.lesson02;
 
+import java.util.Scanner;
+
 /*
 Ускорение свободного падения на Земле и Марсе таково:
 Марс   3.86
@@ -30,5 +32,17 @@ package by.it.akhmelev.lesson02;
 
 */
 class TaskC3 {
+
+    static double getWeight(int me) {
+        return Math.round(me / 9.81 * 3.86*100)/100.0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int me = scanner.nextInt();
+        double mm = getWeight(me);
+        System.out.println(mm);
+
+    }
 
 }
