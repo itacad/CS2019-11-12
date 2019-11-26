2ae87ee5ac16ac54cd3d8a72310fae571c17dfdd
DrCarpediem <57077162+DrCarpediem@users.noreply.github.com>
2019-11-26 13:47:23 +0300

    lesson5 Task B1 complete

--- src/by/it/valuevich/lesson05/TaskB1.java
+++ src/by/it/valuevich/lesson05/TaskB1.java
@@ -10,6 +10,24 @@ package by.it.valuevich.lesson05;
 */
 
 
+import java.util.ArrayList;
+
 public class TaskB1 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
+
+
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
 
 }
