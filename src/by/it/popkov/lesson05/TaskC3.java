d0428d8f4dcd4f62cd58d3ef2a0f8305dc71be08
user <abc>
2019-11-27 10:06:46 +0300

    С3 first try

--- src/by/it/popkov/lesson05/TaskC3.java
+++ src/by/it/popkov/lesson05/TaskC3.java
@@ -5,15 +5,72 @@ package by.it.popkov.lesson05;
 и выводит их в убывающем порядке. Скорость O(n log n)
 */
 
+import java.util.Scanner;
+
 public class TaskC3 {
     //Напишите тут ваш код
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        sort(array, 0, 19);
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
 
 
+    /* The main function that implements QuickSort()
+              arr[] --> Array to be sorted,
+              low  --> Starting index,
+              high  --> Ending index */
+    public static void sort(int arr[], int low, int high) {
+        if (low < high) {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, pi + 1, high);
+            sort(arr, low, pi - 1);
+
+        }
+    }
+
+    /*This code is contributed by Rajat Mishra */
 
 
+    /* This function takes last element as pivot,
+               places the pivot element at its correct
+               position in sorted array, and places all
+               smaller (smaller than pivot) to left of
+               pivot and all greater elements to right
+               of pivot */
+   static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1); // index of smaller element
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot) {
+                i++;
 
-    public static void sort(int[] array) {
-        //Напишите тут ваш код
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
     }
 
 }
