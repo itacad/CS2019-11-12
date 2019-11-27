bbd7e62999b39754ec8346eba1258e3ec966d111
Виктор <lucckyboy713@gmail.com>
2019-11-27 14:28:15 +0300

    sdgsdg

--- src/by/it/givi/lesson05/TaskA3.java
+++ src/by/it/givi/lesson05/TaskA3.java
@@ -23,7 +23,7 @@ public class TaskA3 {
         }
         int min=array[0];
         int max=array[0];
-        for (int i = 1; i < array.length-1; i++){
+        for (int i = 0; i < 10; i++){
             if (array[i] < min) min = array[i];
             if (array[i] > max) max = array[i];
         }
