ab507a9d26e278f35168f6ac1440d0da53f605c1
user <abc>
2019-11-27 10:58:58 +0300

    С3 is done

--- src/by/it/popkov/lesson05/TaskC3.java
+++ src/by/it/popkov/lesson05/TaskC3.java
@@ -15,62 +15,55 @@ public class TaskC3 {
         for (int i = 0; i < array.length; i++) {
             array[i] = scanner.nextInt();
         }
-        sort(array, 0, 19);
-        for (int i : array) {
-            System.out.println(i);
+        sort(array);
+        for (int i = 19; i >= 0; i--) {
+            System.out.println(array[i]);
         }
     }
 
 
-    /* The main function that implements QuickSort()
-              arr[] --> Array to be sorted,
-              low  --> Starting index,
-              high  --> Ending index */
-    public static void sort(int arr[], int low, int high) {
-        if (low < high) {
-            /* pi is partitioning index, arr[pi] is
-              now at right place */
-            int pi = partition(arr, low, high);
-
-            // Recursively sort elements before
-            // partition and after partition
-            sort(arr, pi + 1, high);
-            sort(arr, low, pi - 1);
-
-        }
-    }
-
-    /*This code is contributed by Rajat Mishra */
 
 
-    /* This function takes last element as pivot,
-               places the pivot element at its correct
-               position in sorted array, and places all
-               smaller (smaller than pivot) to left of
-               pivot and all greater elements to right
-               of pivot */
-   static int partition(int arr[], int low, int high) {
-        int pivot = arr[high];
-        int i = (low - 1); // index of smaller element
-        for (int j = low; j < high; j++) {
-            // If current element is smaller than or
-            // equal to pivot
-            if (arr[j] <= pivot) {
-                i++;
+    public static void sort(int[] array) {
+        //Напишите тут ваш код
+        if (array.length == 0) return;
 
-                // swap arr[i] and arr[j]
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+        // Строим кучу
+        int length = array.length;
+        // проходим от первого без ответвлений к корню
+        for (int i = length / 2-1; i >= 0; i--)
+            heapify(array, length, i);
+
+        for (int i = length-1; i >= 0; i--) {
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            heapify(array, i, 0);
         }
 
-        // swap arr[i+1] and arr[high] (or pivot)
-        int temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
     }
+    static void heapify(int[] array, int length, int i) {
+        int leftChild = 2*i+1;
+        int rightChild = 2*i+2;
+        int largest = i;
 
+        // если левый дочерний больше родительского
+        if (leftChild < length && array[leftChild] > array[largest]) {
+            largest = leftChild;
+        }
+
+        // если правый дочерний больше родительского
+        if (rightChild < length && array[rightChild] > array[largest]) {
+            largest = rightChild;
+        }
+
+        // если должна произойти замена
+        if (largest != i) {
+            int temp = array[i];
+            array[i] = array[largest];
+            array[largest] = temp;
+            heapify(array, length, largest);
+        }
+    }
 }
