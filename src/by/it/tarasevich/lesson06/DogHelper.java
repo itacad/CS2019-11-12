def625830b659c527c23bd4f97abbf7663957f45
tarasevich <yuliya6149116@gmail.com>
2019-11-28 12:15:51 +0300

    les6

---
+++ src/by/it/tarasevich/lesson06/DogHelper.java
@@ -0,0 +1,19 @@
+package by.it.tarasevich.lesson06;
+
+public class DogHelper {
+    static void printAllNames(Dog[] dogs) {
+        for (Dog dog : dogs) {
+            System.out.print(dog.getName() + " ");
+        }
+        System.out.println();
+    }
+
+    double averageAge(Dog[] dogs) {
+        double sum = 0;
+        for (Dog dog : dogs) {
+            sum += dog.getAge();
+        }
+        return sum / dogs.length;
+    }
+}
+
