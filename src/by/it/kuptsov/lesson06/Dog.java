f58c2ef53bb07b9e13de7e39476a6df2c52269dc
kuptsov <akuptsovvv@gmail.com>
2019-11-28 12:15:32 +0300

    add lesson 6

---
+++ src/by/it/kuptsov/lesson06/Dog.java
@@ -0,0 +1,32 @@
+package by.it.kuptsov.lesson06;
+
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog(String name, int age) {
+        this name
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Кличка: """
+    }
+}
+
