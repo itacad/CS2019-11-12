3ae0653729e61ae2c3fe3908880ab9aba16d8dce
Виктор <lucckyboy713@gmail.com>
2019-11-28 07:29:35 +0300

    sdgsdg

--- src/by/it/givi/lesson04/TaskC2.java
+++ src/by/it/givi/lesson04/TaskC2.java
@@ -1,5 +1,7 @@
 package by.it.givi.lesson04;
 
+import java.util.Scanner;
+
 /*
 
 На входе вводится число и месяц 2018 года.
@@ -27,5 +29,26 @@ package by.it.givi.lesson04;
 
  */
 public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int day = scan.nextInt();
+        int month = scan.nextInt();
+        int mDay = 0;
+        for (int i = 0; i < month; i++) {
+            if(month <= 1){ break; }
+            else{ mDay += allday(i); }
+        }
+        System.out.println((mDay+day));
+    }
 
+    private static int allday(int t){
+        int s = 0;
+        switch (t){
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12: s = 31; break;
+            case 2: s = 28; break;
+            case 4: case 6: case 9: case 11: s = 30; break;
+        }
+        return s;
+    }
 }
+
