f8390a850a187c240344a70720c9e123795ad8ad
zenevich <annettka.gentille@gmail.com>
2019-11-28 12:15:49 +0300

    lesson 6 tasks A,B complete

---
+++ src/by/it/zenevich/lesson06/DogHelper.java
@@ -0,0 +1,22 @@
+package by.it.zenevich.lesson06;
+
+import java.util.Arrays;
+
+public class DogHelper {
+
+    static void printAllNames(Dog[] dogs) {
+        for (Dog dog : dogs) {
+            System.out.print(dog.getName() + " ");
+        }
+        System.out.println();
+    }
+
+    static double averageAge(Dog[] dogs) {
+        double sum = 0;
+        for (Dog dog : dogs) {
+            sum += dog.getAge();
+        }
+        return sum / dogs.length;
+
+    }
+}
