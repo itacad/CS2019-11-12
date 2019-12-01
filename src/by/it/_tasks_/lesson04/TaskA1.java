66345683cddd6db7911e648436389b3bc69c953f
Alexander Khmelov <akhmelev@gmail.com>
2019-12-01 23:17:45 +0300

    fix

--- src/by/it/_tasks_/lesson04/TaskA1.java
+++ src/by/it/_tasks_/lesson04/TaskA1.java
@@ -25,14 +25,6 @@ package by.it._tasks_.lesson04;
 
  */
 public class TaskA1 {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            if (i != 3 && i != 9) {
-                System.out.println("Я изучаю программирование!");
-            } else {
-                System.out.println("Меня интересует высокая зарплата!");
-            }
-        }
-    }
+
 
 }
