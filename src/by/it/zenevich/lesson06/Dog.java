2ae796bed9631c2ef2a1b4acd6f21eaac86be680
zenevich <annettka.gentille@gmail.com>
2019-12-01 16:03:22 +0300

    lesson 6 tasks A,B,C complete

--- src/by/it/zenevich/lesson06/Dog.java
+++ src/by/it/zenevich/lesson06/Dog.java
@@ -3,6 +3,8 @@ package by.it.zenevich.lesson06;
 class Dog {
     private String name;
     private int age;
+    private int weight;
+    private double power;
 
     public Dog() {
     }
@@ -10,6 +12,14 @@ class Dog {
     public Dog(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public Dog(String name, int age, int weight, double power) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.power = power;
+
     }
 
     public String getName() {
@@ -27,6 +37,31 @@ class Dog {
     public void setAge(int age) {
         this.age = age;
     }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+    boolean win(Dog otherDog) {
+//        Шансы на победу = 0.2 * возраст + 0.3 * вес + 0.5 * силу укуса.
+        double thisChance = 0.2 * this.age + 0.3 * this.weight + 0.5 * this.power;
+        double otherDogChance = 0.2 * otherDog.age + 0.3 * otherDog.weight + 0.5 * otherDog.power;
+
+        return thisChance > otherDogChance;
+    }
+
 
     @Override
     public String toString() {
